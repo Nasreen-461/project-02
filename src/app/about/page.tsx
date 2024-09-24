172b70e30b4a2ef70535a96bcbb92a7ea4f37af8
Nasreen-461 <nasreenrafiq461@gmail.com>
2024-09-24 16:30:01 +0500

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>This is my about page work</p>
+      </div>
+    )
+  }
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Hello World . Hello next.js!</h1>
+        <p>This is my contact page work </p>
+      </div>
+    )
+  }
